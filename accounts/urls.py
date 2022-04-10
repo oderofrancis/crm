e6fb0db715bf3543de0960d59ff5cae0bf7c81c6
oderofrancis <franodex10@gmail.com>
2022-04-10 22:32:39 +0300

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',home,name='home'),
+    path('products/',products,name='products'),
+    path('customers/',customers,name='customers'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	return render(request,'accounts/home.html')
+
+def products(request):
+	return render(request,'accounts/products.html')
+
+def customers(request):
+	return render(request,'accounts/customers.html')
