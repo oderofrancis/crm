c2f8ab5958f30d5d60946b41a63ea4a0541dd9ea
oderofrancis <franodex10.com>
2022-04-19 01:09:16 +0300

    one to one use

--- accounts/views.py
+++ accounts/views.py
@@ -25,6 +25,9 @@ def registerPage(request):
 
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
+			Customer.objects.create(
+				user=user
+				)
 
 			messages.success(request, 'Account was created for' + username)
 
@@ -81,8 +84,19 @@ def home(request):
 
 # for user-page
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+
 def userPage(request):
-	context = {}
+	orders = request.user.customer.order_set.all()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	context = {'orders':orders,'total_orders':total_orders,
+				'delivered':delivered,'pending':pending
+				}
 	return render(request,'accounts/user.html',context)
 
 @login_required(login_url='login')
